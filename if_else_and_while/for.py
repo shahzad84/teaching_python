5a5c096986431763cdfdf64ccb9dfdd308617fd3
shahzad84 <shahzad20022002@gmail.com>
2024-02-03 12:24:51 +0530

    for loop

---
+++ if_else_and_while/for.py
@@ -0,0 +1,26 @@
+
+# for loop is used to iterate each character of a string or item of a collection
+
+# table
+# num=2
+# for i in range(1,11):
+#     print(num*i)
+
+
+# calculate total price
+
+# prices=[12,22,11]
+# total=0
+# for price in prices:
+#     total+=price
+# print(total)
+
+
+# making some using nested for loop 
+
+num=[5,6,7,8,7,6,5]
+for x_count in num:
+    output=""
+    for count in range(x_count):
+        output+="x"
+    print(output)
--- if_else_and_while/if_else.py
+++ if_else_and_while/if_else.py
@@ -13,8 +13,3 @@
 
 # and or operator
     
-# table
-
-num=2
-for i in range(1,11):
-    print(num*i)
