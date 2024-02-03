e09496d674383c323608d2edd588655ddd550f12
shahzad84 <shahzad20022002@gmail.com>
2024-02-03 11:52:19 +0530

    if else and while loop

---
+++ if_else_and_while/if_else.py
@@ -0,0 +1,20 @@
+# is_hot=False
+# is_cold=True
+
+# if is_hot:
+#     print("its hot day")
+#     print("drink water")
+# elif is_cold:
+#     print("its cold day")
+#     print("wear warm cloths")
+# else:
+#     print("its s lovely day")
+
+
+# and or operator
+    
+# table
+
+num=2
+for i in range(1,11):
+    print(num*i)
---
+++ if_else_and_while/while.py
@@ -0,0 +1,9 @@
+# while loop execute a block of code multiple time
+
+number=2
+i=1
+
+while i<=10:
+    result=number*i
+    print(f"{number} * {i} = {result}")
+    i+=1
